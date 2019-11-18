456b6247fe21641dfa069a3b444dcf68085a3c7e
EricFerguson76 <turtle0888@gmail.com>
2019-11-18 17:37:49 -0600

    Added Parent Reg form

--- kidsfly/src/App.js
+++ kidsfly/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import FormikForms from './Register/ParentReg';
 
 function App() {
 
   return (
     <div className="App">
-
+      <FormikForms />
     </div>
   )
 }
---
+++ kidsfly/src/Register/ParentReg.js
@@ -0,0 +1,82 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { withFormik, Form, Field } from 'formik';
+import * as Yup from 'yup';
+import styled from 'styled-components';
+
+
+const ParentReg = ({ errors, touched, values, status }) => {
+
+  const [travelers, setTravelers] = useState({ email: '', password: '', home_airport: '' });
+
+  return (
+    <div>
+      <h1>KidsFly Parent Registration</h1>
+      <Form>
+        <Field
+          type='text'
+          name='email'
+          placeholder='Email'
+        />
+        {touched.name && errors.email && (
+          <p className='error'>{errors.email}</p>
+        )}
+
+        <Field
+          type='password'
+          name='password'
+          placeholder='Password'
+        />
+        {touched.name && errors.password && (
+          <p className='error'>{errors.password}</p>
+        )}
+
+        <Field
+          type='text'
+          name='home_airport'
+          placeholder='Home Airport'
+        />
+        {touched.name && errors.home_airport && (
+          <p className='error'>{errors.home_airport}</p>
+        )}
+
+        <button type='submit'>Submit</button>
+      </Form>
+
+    </div>
+  );
+};
+const FormikForms = withFormik({
+  mapPropsToValues({ email, password, home_airport }) {
+    return {
+      email: email || '',
+      password: password || '',
+      home_airport: home_airport || ''
+    }
+  },
+
+  validationSchema: Yup.object().shape({
+    email: Yup.string().required('email required'),
+    password: Yup.string().required('password required'),
+    home_airport: Yup.string().required('airport required')
+
+
+  }),
+
+  handleSubmit(values) {
+    console.log(values)
+    axios.post('https://kidsfly-be-dakotah.herokuapp.com/api/auth/register', values)
+      .then(response => {
+
+        console.log(response)
+      })
+      .catch(err => console.log(err))
+  }
+
+
+
+
+})(ParentReg)
+
+
+export default FormikForms;
