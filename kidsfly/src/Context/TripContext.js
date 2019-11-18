fce08079ef2f20732338e8679c7b17a5fc14cbf2
Kyle Corbin <kylecorbin26@gmail.com>
2019-11-18 16:28:45 -0500

    Switched from session to local storage

--- kidsfly/src/Context/TripContext.js
+++ kidsfly/src/Context/TripContext.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 export const TripContext = React.createContext();
 
 export const TripProvider = props=>{
-    const [isLoggedIn, setLoggedIn] = useState(sessionStorage.getItem('token')? true: false)
+    const [isLoggedIn, setLoggedIn] = useState(localStorage.getItem('token')? true: false)
     //Honestly think Trips should just be an array since out travelers and careTakers are going to loop through them to create their dashboards
     const [trips, setTrips] = useState([]);
 
