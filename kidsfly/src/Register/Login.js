00d4bc278d85a34b4e16c68428d8760a47e11768
ryankayne <coope1rk@gmail.com>
2019-11-18 17:34:23 -0500

    Formik Forms w Axios

--- kidsfly/src/Register/Login.js
+++ kidsfly/src/Register/Login.js
@@ -5,25 +5,25 @@ import axios from "axios";
 
 const Login = ({ values, errors, touched, status }) => {
   // const Login = ({ errors, touched, values }) => {
-  const [user, setUser] = useState([]);
-  useEffect(() => {
-    status && setUser(user => [...user, status]);
-  }, [status]);
+  const [user, setUser] = useState({ email: "", password: "" });
+//   useEffect(() => {
+//     status && setUser(user => [...user, status]);
+//   }, [status]);
 
-  const handleChange = event => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  };
+//   const handleChange = event => {
+//     setUser({ ...user, [event.target.name]: event.target.value });
+//   };
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    console.log(user.username);
-  };
+//   const handleSubmit = event => {
+//     event.preventDefault();
+//     console.log(user);
+//   };
 
   return (
     <div className="login-form">
       <Form>
           Enter Login
-        <Field type="text" name="username" placeholder="Username" />
+        <Field type="text" name="email" placeholder="Email" />
         {touched.username && errors.username && (
           <p className="errors">{errors.username}</p>
         )}
@@ -32,35 +32,41 @@ const Login = ({ values, errors, touched, status }) => {
         {touched.password && errors.password && <p className="errors">{errors.password}</p>}
         <button>Submit</button>
       </Form>
-      {user.map(user => (
+      {/* {user.map(user => (
         <ul key={user.id}>
           <li>Username: {user.username}</li>
           <li>Password: {user.password}</li>
         </ul>
-      ))}
+      ))} */}
     </div>
   );
 };
 
 const FormikLogin = withFormik({
-  mapPropsToValues({ user, password }) {
+  mapPropsToValues({ email, password }) {
     return {
-      user: user || "",
+      email: email || "",
       password: password || ""
     };
   },
   validationSchema: Yup.object().shape({
-    user: Yup.string().required(),
+    email: Yup.string().required(),
     password: Yup.string().required()
   }),
-  handleSubmit(values, { setStatus }) {
+
+  handleSubmit(values) {
+      console.log("from formik", values)
     axios
-      .post("https://reqres.in/api/users/", values)
+      .post("https://kidsfly-be-dakotah.herokuapp.com/api/auth/login", values)
       .then(res => {
-        setStatus(res.data);
+        // setStatus(res.data);
         console.log(res);
+        localStorage.setItem("token", res.data.token)
       })
-      .catch(err => console.log(err.response));
+      .catch(err => console.log(err.response))
+      .finally(() => {
+          console.log('From Local Storage', localStorage.getItem("token"))
+      })
   }
 })(Login);
 
