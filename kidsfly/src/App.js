8bca18bf38cd8274fabd1b970fe47656489b04d6
ryankayne <coope1rk@gmail.com>
2019-11-18 14:01:07 -0500

    Refactored old code to get form fields

--- kidsfly/src/App.js
+++ kidsfly/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import './App.css';
+import Login from "./Register/Login";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Login />
       </header>
     </div>
   );
---
+++ kidsfly/src/Register/Login.js
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+
+const Login = ({ values, errors, touched, status }) => {
+  // const Login = ({ errors, touched, values }) => {
+  const [user, setUser] = useState([]);
+  useEffect(() => {
+    status && setUser(user => [...user, status]);
+  }, [status]);
+
+  const handleChange = event => {
+    setUser({ ...user, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = event => {
+    event.preventDefault();
+    console.log(user.username);
+  };
+
+  return (
+    <div className="login-form">
+      <Form>
+        <Field type="text" name="username" placeholder="Username" />
+        {touched.username && errors.username && (
+          <p className="errors">{errors.username}</p>
+        )}
+        <Field type="password" name="password" placeholder="Password" />
+        {touched.password && errors.password && <p className="errors">{errors.password}</p>}
+        <button>Submit</button>
+      </Form>
+      {user.map(user => (
+        <ul key={user.id}>
+          <li>Username: {user.username}</li>
+          <li>Password: {user.password}</li>
+        </ul>
+      ))}
+    </div>
+  );
+};
+
+const FormikLogin = withFormik({
+  mapPropsToValues({ user, password }) {
+    return {
+      user: user || "",
+      password: password || ""
+    };
+  },
+  validationSchema: Yup.object().shape({
+    user: Yup.string().required(),
+    password: Yup.string().required()
+  }),
+  handleSubmit(values, { setStatus }) {
+    axios
+      .post("https://reqres.in/api/users/", values)
+      .then(res => {
+        setStatus(res.data);
+        console.log(res);
+      })
+      .catch(err => console.log(err.response));
+  }
+})(Login);
+
+export default FormikLogin;
