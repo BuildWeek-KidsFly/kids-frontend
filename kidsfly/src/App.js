cfd53e7f5142d0b630316e713e89bfb7dfef6e20
Kyle Corbin <kylecorbin26@gmail.com>
2019-11-18 19:58:48 -0500

    resolution

--- kidsfly/src/App.js
+++ kidsfly/src/App.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import NavBar from './Components/Navbar';
+
 import Login from "./Register/Login";
-import {Switch, Route} from "react-router-dom";
+
 
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <NavBar/>
 
-    </div>
-  );
-}
 
 export default App;
