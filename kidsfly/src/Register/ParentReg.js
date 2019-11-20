1dd4c14eed17af213b6a56a928426e423b8f8feb
EricFerguson76 <turtle0888@gmail.com>
2019-11-20 14:32:02 -0600

    Added styling and links

--- kidsfly/src/Register/ParentReg.js
+++ kidsfly/src/Register/ParentReg.js
@@ -2,35 +2,34 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import { withFormik, Form, Field } from 'formik';
 import * as Yup from 'yup';
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components';
 
 
 const Container = styled.div`
-margin-left: 60px;
-margin-right:60px;
 display: flex;
 justify-content: center;
-padding-top: 60px;
+padding-top: 80px;
 background-image: url('https://images.unsplash.com/photo-1415959588285-66bbf7450d17?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60');
-background-color: black;
 background-position: center;
 background-repeat: no-repeat;
 background-size: cover;
-
+height: 100%;
+margin-left: 100px;
+margin-right: 100px;
 `;
 
 const Card = styled.div`
-background: #999999;
+background: #091d86;
 width: 400px;
 max-height: 400px;
 display: flex;
 flex-direction: column;
 align-items: center;
 justify-content: space-between;
-margin-bottom: 20px;
-box-shadow: 5px 5px #666666 ;
-border: 2px solid white;
+margin-bottom: 80px;
+box-shadow: 5px 5px black ;
+border: 2px solid #999999;
 color: #ffcc00;
 `;
 
@@ -51,6 +50,7 @@ const Button = styled.button`
   cursor: pointer;
 
 `;
+
 
 
 const ParentReg = (props) => {
@@ -91,7 +91,7 @@ const ParentReg = (props) => {
               )}
             </Box>
           </div>
-          <div>
+          {/* <div>
             <label htmlFor="home_airport">Home Airport</label>
             <Box>
               <Field
@@ -103,9 +103,9 @@ const ParentReg = (props) => {
                 <p className='error'>{props.errors.home_airport}</p>
               )}
             </Box>
-          </div>
+          </div> */}
           <Button type='submit'>Submit</Button>
-          <p>Already have an Account? Click Here</p>
+          <p>Already have an Account? <NavLink to='/login'>Click Here</NavLink></p>
         </Form>
 
       </Card>
