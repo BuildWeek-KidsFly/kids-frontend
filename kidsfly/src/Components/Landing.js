75ef794681980069e385a23e8e862c7775ff2ded
ryankayne <coope1rk@gmail.com>
2019-11-21 17:27:21 -0500

    Responsive Landing Page

--- kidsfly/src/Components/Landing.js
+++ kidsfly/src/Components/Landing.js
@@ -23,6 +23,14 @@ const Landing = () => {
         text-shadow: 4px 4px 1px black;
         -webkit-text-stroke-width: 1px;
         -webkit-text-stroke-color: black;
+
+        @media (max-width: 800px) {
+            font-size: 3rem;
+        }
+
+        @media (max-width: 500px) {
+            font-size: 2.5rem;
+        }
     `;
 
     const Banner = styled.div`
@@ -32,18 +40,28 @@ const Landing = () => {
         -o-background-size: cover;
         background-size: cover;
         height: 100vh;
+
+        @media (max-width: 500px) {
+            height: 120vh;
+        }
     `;
 
     const Holder = styled.div`
         display: flex;
-        justify-content: space-around;        
+        justify-content: space-around;  
+        
+        @media (max-width: 1000px) {
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+        }
         
     `;
 
     const Container = styled.div`
         background: #091D86;
         width: 30%;
-        height: 275px;
+        height: 17em;
         border: solid black 1px;
         border-radius: 20px;
         margin-top: 4%;
@@ -54,18 +72,36 @@ const Landing = () => {
         align-items: center;
         flex-direction: column;
 
-        // @media (max-width: 800px) {
-        //     background-color: black;
-        // }
+        @media (max-width: 1000px) {
+            width: 70%;
+            height: 15em;
+            margin-top: 2%;
+    
+        }
+
+        @media (max-width: 500px) {
+            height: 16em;
+            margin-bottom: 2em;
+        }
     `;
 
     const Header2 = styled.h2`
         color: #FFCC00;
         font-family: 'Quicksand', sans-serif;
+
+        @media (max-width: 500px) {
+            font-size: 1em;
+        }
         `;
+
     const TextContain = styled.div`
         max-height: 70px;
         max-width: 400px;
+
+        @media (max-width: 1000px) {
+            max-height: 80px;
+            max-width: 550px;
+        }
     `;
 
     const Paragraph = styled.p`
@@ -73,21 +109,43 @@ const Landing = () => {
         margin-top: 0;
         letter-spacing: 2px;
         font-family: 'Quicksand', sans-serif;
+        padding: 10px;
+
+        @media (max-width: 500px) {
+            padding: 0;
+            font-size: .9em;
+        }
         `;
 
     const Button = styled.button`
         background: #FFCC00;
         border-radius: 20px;
-        height: 60px;
-        width: 170px;
+        height: 3em;
+        width: 9em;
         margin-top: 25%;
         border: solid black 2px;
         font-weight: bold;
-        font-size: 1.2rem;
+        font-size: 1.2em;
         :hover {
             background: #23972A;
             cursor: pointer;
             box-shadow: 3px 3px 3px black;
+        }
+
+        @media (max-width: 1000px) {
+            margin-top: 0;
+        }
+
+        @media (max-width: 800px) {
+            margin-top: 10%;
+            height: 2.8em;
+            width: 8em;
+            font-size: 1em;
+            border-radius: 17px;
+        }
+
+        @media (max-width: 500px) {
+            margin-top: 30px;
         }
     `;
 
--- kidsfly/src/Components/Navbar.js
+++ kidsfly/src/Components/Navbar.js
@@ -2,17 +2,10 @@ import React from 'react';
 import styled from "styled-components";
 import {NavLink} from "react-router-dom";
 
-
-import {Link} from "react-router-dom";
-
-
-const NavBar = (props)=>{
-
+const NavBar = ()=>{
     const clear = () => {
         window.location.reload();
         localStorage.clear();
-        
-        props.history.push("/");
     }
    
 const Button = styled.button`
@@ -32,16 +25,13 @@ const Button = styled.button`
     return (
         <div className="Nav">
             <header className="Header">
-
                 <NavLink to="/">Home</NavLink>
                 <NavLink to="/login">Parent Login</NavLink>
                 <NavLink to="/signup">Parent Sign Up</NavLink>
                 <NavLink to="/connectionLog">Connect Login</NavLink>
                 <NavLink to="/connectionReg">Connect Sign Up</NavLink>
                 <NavLink to="/dashboard">Dashboard</NavLink>
-                <NavLink to="/connectdashboard">Connection Dashboard</NavLink>
                 <Button onClick={clear}>Log Out</Button>
-
             </header>
         </div>
     );
